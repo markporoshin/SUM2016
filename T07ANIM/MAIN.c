36deec29bf2fc6c0f460d77e27ccb6b2b07e126e
Poroshin Mark (MP2) <mark.poroshin@yandex.ru>
2016-06-09 18:41:35 +0400

    много чего

--- T07ANIM/MAIN.c
+++ T07ANIM/MAIN.c
@@ -7,8 +7,9 @@
 
 #define WND_CLASS_NAME "My window class"
 
-/* Begin of function "MyWindowFunc" */
-LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,WPARAM wParam, LPARAM lParam)
+
+
+/*LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,WPARAM wParam, LPARAM lParam)
 {         
   static BITMAP bm;
   static HBITMAP hBm;
@@ -29,11 +30,11 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,WPARAM wParam, LPARAM lParam)
     return 0;
 
   case WM_TIMER: 
-    /*SelectObject(hDC, GetStockObject(DC_PEN));
+    SelectObject(hDC, GetStockObject(DC_PEN));
     SelectObject(hDC, GetStockObject(DC_BRUSH));
     SetDCPenColor(hDC, RGB(100,150,0));
     SetDCBrushColor(hDC, RGB(100,150,0));
-    Rectangle(hMemDC, 0, 0, w+1, h+1);*/
+    Rectangle(hMemDC, 0, 0, w+1, h+1);
     
     InvalidateRect(hWnd, NULL, FALSE);
     return 0;
@@ -72,57 +73,29 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,WPARAM wParam, LPARAM lParam)
     return 0;
   }
   return DefWindowProc(hWnd, Msg, wParam, lParam);
-}/* End of "MyWindowFunc" */
+}*/ 
+
 
 /* Main program function */
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd )
 {
-  WNDCLASS wc;
-  //HWND hWnd;
-  MSG msg;
+ DBL sum;
+ MATR m;
+ VEC A, B, C;
 
-  wc.style = CS_VREDRAW | CS_HREDRAW;
-  wc.cbClsExtra = 0;
-  wc.cbWndExtra = 0;
-  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-  wc.lpszMenuName = NULL;
-  wc.hInstance = hInstance;
-  wc.lpfnWndProc = MyWindowFunc;
-  wc.lpszClassName = WND_CLASS_NAME;
-  
-  if(!RegisterClass(&wc))
-  {
-    MessageBox(NULL,"Error register window class","Error",MB_OK);
-    return 0;
-  }
+ A = VecSet(1, 0, 0);
+ B = VecSet(0, 1, 0);
+ C = VecCrossVec(A, B);
+ C = VecCrossVec(B, A);
+ sum = VecDotVec(A, B);
 
-  /*hWnd = CreateWindow(WND_CLASS_NAME,"EYES",
-                      WS_OVERLAPPEDWINDOW,
-                      CW_USEDEFAULT,CW_USEDEFAULT,
-                      CW_USEDEFAULT,CW_USEDEFAULT,
-                      NULL,
-                      NULL,
-                      hInstance,
-                      NULL);
-   CreateWindow("Button","ON/OFF",
-                      WS_CHILDWINDOW | WS_VISIBLE,
-                      CW_USEDEFAULT,CW_USEDEFAULT,
-                      60,30,
-                      hWnd,
-                      NULL,
-                      hInstance,
-                      NULL);
-  ShowWindow(hWnd, SW_SHOWNORMAL); 
-  
-  UpdateWindow(hWnd);  */
-  while (GetMessage(&msg, NULL, 0, 0))
-  {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
-  return 0;
+
+
+ m = MatrIdentity();
+ m = MatrMulMatr(MatrTranslate(VecSet(1, 2, 3)), MatrTranslate(VecSet(1, 2, 3)));
+ m = MatrTranspose(m);
+ //m = MatrMulMatr(m, MatrInverse(m));
+ m = MatrMulMatr(m, MatrRotateX(0));
 } /* End of Main function */
 
 /* End of file "MAIN.c" */
