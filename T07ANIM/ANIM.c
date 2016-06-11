47f7a8119889466fc827e8819e55c0eb3d00c634
Poroshin Mark (MP2) <mark.poroshin@yandex.ru>
2016-06-11 10:09:34 +0400

    timer update

--- T07ANIM/ANIM.c
+++ T07ANIM/ANIM.c
@@ -6,16 +6,31 @@
 #include "ANIM.H"
 
 mp2ANIM MP2_Anim;
+static UINT64
+  MP2_StartTime,  /* Start program time */
+  MP2_OldTime,    /* Time from program start to previous frame */
+  MP2_OldTimeFPS, /* Old time FPS measurement */
+  MP2_PauseTime,  /* Time during pause period */
+  MP2_TimePerSec, /* Timer resolution */
+  MP2_FrameCounter; /* Frames counter */
 
 VOID MP2_AnimInit( HWND hWnd )
 { 
   HDC hDC;
+  LARGE_INTEGER t;
   MP2_Anim.hWnd = hWnd;
   SetTimer(hWnd, 0, 10, NULL);
   hDC = GetDC(hWnd);
   MP2_Anim.hDC = CreateCompatibleDC(hDC);
   ReleaseDC(hWnd, hDC);
   MP2_Anim.NumOfUnits = 0;
+  
+    /*** init timer ***/
+  QueryPerformanceFrequency(&t);
+  MP2_TimePerSec = t.QuadPart;
+  QueryPerformanceCounter(&t);
+  MP2_StartTime = MP2_OldTime = MP2_OldTimeFPS = t.QuadPart;
+  MP2_PauseTime = 0;
 }
 
 VOID MP2_AnimResize( INT w,INT h )
@@ -63,7 +78,39 @@ VOID MP2_AnimClose( VOID )
 VOID MP2_AnimRender( VOID )
 {
   INT i;
+  LARGE_INTEGER t;
   srand(1);
+  
+    /*** Handle timer ***/
+  MP2_FrameCounter++;
+  QueryPerformanceCounter(&t);
+  /* Global time */
+  MP2_Anim.GlobalTime = (DBL)(t.QuadPart - MP2_StartTime) / MP2_TimePerSec;
+  MP2_Anim.GlobalDeltaTime = (DBL)(t.QuadPart - MP2_OldTime) / MP2_TimePerSec;
+  /* Time with pause */
+  if (MP2_Anim.IsPause)
+  {
+    MP2_Anim.DeltaTime = 0;
+    MP2_PauseTime += t.QuadPart - MP2_OldTime;
+  }
+  else
+  {
+    MP2_Anim.DeltaTime = MP2_Anim.GlobalDeltaTime;
+    MP2_Anim.Time = (DBL)(t.QuadPart - MP2_PauseTime - MP2_StartTime) / MP2_TimePerSec;
+  }
+  /* FPS */
+  if (t.QuadPart - MP2_OldTimeFPS > MP2_TimePerSec)
+  {
+    CHAR str[100];
+
+    MP2_Anim.FPS = MP2_FrameCounter * MP2_TimePerSec /
+                                         (DBL)(t.QuadPart - MP2_OldTimeFPS);
+    MP2_OldTimeFPS = t.QuadPart;
+    MP2_FrameCounter = 0;
+    sprintf(str, "FPS: %.5f", MP2_Anim.FPS);
+    SetWindowText(MP2_Anim.hWnd, str);
+  }
+  MP2_OldTime = t.QuadPart;
   /*. . . опросили все (kbd, mouse, joystick)*/   SelectObject(MP2_Anim.hDC, GetStockObject(DC_PEN));
   SelectObject(MP2_Anim.hDC, GetStockObject(DC_BRUSH));
   SetDCPenColor(MP2_Anim.hDC, RGB(100,150,200));
